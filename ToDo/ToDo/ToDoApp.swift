a342fd2fd6243b1c83354989b851ba73f0f55274
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-07-25 17:52:11 +0300

    added git ignor

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Khusrav Safiev on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
